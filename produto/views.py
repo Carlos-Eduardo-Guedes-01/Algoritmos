900575261a1d4f7c77a6bca08f95907d84ee497e
Carlos-Eduardo-Guedes-01 <carloseduardoguedes89981456761@gmail.com>
2023-05-09 09:10:54 -0300

    update lista empresas

--- produto/views.py
+++ produto/views.py
@@ -13,7 +13,12 @@ def cad_prod(request):
     my_object = Empresa.objects.get(pk=24)
     if(request.POST):
         data['form']=ProdutoForm(request.POST, request.FILES)
+        form=data['form']
         if(data['form'].is_valid()):
+            nome=Empresa.objects.get(id=request.POST.get('empresa'))
+            quant=nome.quant_prod
+            quant+=1
+            teste=Empresa.objects.filter(nome_empresa=nome).update(quant_prod=quant)
             produto = data['form'].save(commit=False)
             img = request.FILES
             dados_img = imghdr.what(img['imagem'])
